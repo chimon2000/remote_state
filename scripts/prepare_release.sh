81d9c3096d0ca695badae0b5c3016bb6c545955d
Ryan <ryan.c.edge@outlook.com>
2020-04-21 03:33:31 -0400

    feat: add script to prepare release

---
+++ scripts/prepare_release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e # abort CI if an error happens
+
+echo -n "Select change type and press [ENTER]: 
+"
+
+PS3="Select a change type: "
+workingDirectory=./packages/remote_state
+
+select version in minor major
+do
+    if [ -z "${version}" ]; then
+    echo "Missing or incorrect change type"
+    exit 1
+    fi
+    cd $workingDirectory
+    version=$(pubver bump $version)
+    cd -
+    git-chglog --next-tag $version -o CHANGELOG.md
+    exit
+done
