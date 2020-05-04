702396c81bc6d3b56f6fbdd3ba8e9b9af3c5e689
Ryan <ryan.c.edge@outlook.com>
2020-05-03 23:07:56 -0400

    feat: updated examples

--- examples/counter_state_notifier/lib/home.dart
+++ examples/counter_state_notifier/lib/home.dart
@@ -27,10 +27,9 @@ class HomePage extends StatelessWidget {
             //3. Render state changes
             counterState.when(
               initial: () => Text('Not loaded', style: textStyle),
-              empty: () => Text('Never', style: textStyle),
               success: (value) => Text('$value', style: textStyle),
               loading: () => Text('Loading...', style: textStyle),
-              error: (_) => Text('Error', style: textStyle),
+              error: (_, __) => Text('Error', style: textStyle),
             ),
           ],
         ),
