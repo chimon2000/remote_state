51ad88baf7932d4836433405cc97f9eede984e26
Ryan <ryan.c.edge@outlook.com>
2020-05-05 12:50:48 -0400

    refactor: state notifier hook per suggestions.

--- examples/counter_hooks/lib/home.dart
+++ examples/counter_hooks/lib/home.dart
@@ -1,9 +1,8 @@
-import 'package:counter_hooks/counter/counter_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-import 'counter/counter_notifier.dart';
-import 'hooks/state_notifier.dart';
+import 'package:counter_hooks/counter/counter_notifier.dart';
+import 'package:counter_hooks/hooks/state_notifier.dart';
 
 class HomePage extends HookWidget {
   const HomePage({Key key}) : super(key: key);
